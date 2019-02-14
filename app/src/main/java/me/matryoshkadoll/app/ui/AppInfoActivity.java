cabb18ff13f29f867476640145fb40d629b7b6aa
oscar l <oscar88feichang6@gmail.com>
2019-02-14 17:30:44 -0600

    download success

--- app/src/main/java/me/matryoshkadoll/app/ui/AppInfoActivity.java
+++ app/src/main/java/me/matryoshkadoll/app/ui/AppInfoActivity.java
@@ -1,6 +1,11 @@
 package me.matryoshkadoll.app.ui;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +13,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 import me.matryoshkadoll.app.R;
 
+import static me.matryoshkadoll.app.login.LoginActivity.MY_PREFS_NAME;
+
 public class AppInfoActivity extends AppCompatActivity {
     private TextView tvs;
-
+    private Button button;
+    private long downloadID;
+    private  String AppId;
+    private BroadcastReceiver onDownloadComplete = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Fetching the download id received with the broadcast
+            long id = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+            //Checking if the received broadcast is for our enqueued download by matching download id
+            if (downloadID == id) {
+                Toast.makeText(AppInfoActivity.this, "Download Completed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,22 +47,49 @@ public class AppInfoActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Intent it=getIntent();
-
-
         tvs = (TextView) findViewById(R.id.textView2);
         int tt = it.getIntExtra("app_id",999);
+        tvs.setText(Integer.toString(tt));
+        AppId=Integer.toString(tt);
 
-        Log.i("app id", "app id " + tt);
-        tvs.setText(tt);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        /*fab.setOnClickListener(new View.OnClickListener() {
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                beginDownload();
+
             }
-        });*/
+        });
+
+    }
+// under oncreate
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(onDownloadComplete);
+    }
+    private void beginDownload(){
+        File file=new File(getExternalFilesDir(null),"Dummy");
+        /*
+        Create a DownloadManager.Request with all the information necessary to start the download
+         */
+        String url = "https://matryoshkadoll.me/api/v1/android_apps/"+AppId+"/file";
+        String Bearer = "Bearer ";
+        //fetch token
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String An = prefs.getString("AccessToken", "No name defined");
+
+        DownloadManager.Request request=new DownloadManager.Request(Uri.parse(url))
+                .addRequestHeader("Authorization", Bearer+An)
+                .setTitle("Dummy File.apk")// Title of the Download Notification
+                .setDescription("Downloading")// Description of the Download Notification
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)// Visibility of the download Notification
+                .setDestinationUri(Uri.fromFile(file))// Uri of the destination file
+                .setAllowedOverMetered(true)// Set if download is allowed on Mobile network
+                .setAllowedOverRoaming(true);// Set if download is allowed on roaming network
+        DownloadManager downloadManager= (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        downloadID = downloadManager.enqueue(request);// enqueue puts the download request in the queue.
     }
 
 }
