397cf3dff589b406fc7ef351fdb49010a54403a6
oscar l <oscar88feichang6@gmail.com>
2019-03-01 05:50:48 -0600

    add profile page

---
+++ app/src/main/java/me/matryoshkadoll/app/ui/ProfileActivity.java
@@ -0,0 +1,14 @@
+package me.matryoshkadoll.app.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import me.matryoshkadoll.app.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
