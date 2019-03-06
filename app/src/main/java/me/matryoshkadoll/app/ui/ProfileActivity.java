b2e88beb77d4b5eb5ed3218007a1c8d9a8f33ccc
oscar666666 <oscar88feichang6@gmail.com>
2019-03-05 18:03:50 -0600

    display user name & email

--- app/src/main/java/me/matryoshkadoll/app/ui/ProfileActivity.java
+++ app/src/main/java/me/matryoshkadoll/app/ui/ProfileActivity.java
@@ -5,12 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 import me.matryoshkadoll.app.R;
+import me.matryoshkadoll.app.api.model.UserName;
+import me.matryoshkadoll.app.api.service.matryoshka.AndroidAppsClient;
 import me.matryoshkadoll.app.network.OkHTTPClientInstance;
+import me.matryoshkadoll.app.network.RetrofitClientInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static me.matryoshkadoll.app.login.LoginActivity.MY_PREFS_NAME;
 
@@ -36,6 +43,7 @@ public class ProfileActivity extends AppCompatActivity {
     private void TemplateMethod(){
         FetchToken();
         DisplayProfileImg();
+        DisplayName();
     }
 
     private void FetchToken(){
@@ -53,5 +61,29 @@ public class ProfileActivity extends AppCompatActivity {
                 .build();
         profileimg= (ImageView) findViewById(R.id.profile);
         picasso.load(url).into(profileimg);
+    }
+    private void DisplayName(){
+        name = (TextView)findViewById(R.id.name);
+        email = (TextView)findViewById(R.id.emailh);
+        AndroidAppsClient client = RetrofitClientInstance.getRetrofitInstance().create(AndroidAppsClient.class);
+
+        Call<UserName> calluser = client.GetUserName(An,userid);
+        // HTTP callback
+        calluser.enqueue(new Callback<UserName>() {
+            @Override
+            public void onResponse(Call<UserName> call, Response<UserName> response) {
+                // Get data from response
+                UserName myUserName = response.body();
+                if(myUserName != null){
+                    email.setText(myUserName.getData().getEmail());
+                    name.setText(myUserName.getData().getName());
+                }
+            }
+            @Override
+            public void onFailure(Call<UserName> call, Throwable t) {
+
+            }
+        });
+
     }
 }
